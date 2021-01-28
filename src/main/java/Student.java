43d1c8cc4dcf8bad81c53dcacbfdec3175850252
unknown <natalija_kostovska@hotmail.com>
2021-01-28 14:30:23 +0100

    first commit

---
+++ src/main/java/Student.java
@@ -0,0 +1,24 @@
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+//    kom i jar
+//    build tag-> plagins
+//    confi - > conf tag
+
+
+    //maven clean
+    //mvn package
+    // jdk na java /bin
+    //
+    //java -jar
+    //java - jar
+//
+}
